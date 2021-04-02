247a65f64675ba38f622467ccafd5759020814ed
Ethan <bigetinsley@yahoo.com>
2021-04-01 23:42:44 -0400

    Update Exercise Object

--- app/src/main/java/com/exerciseapplication/finalexerciseapplication/Exercise.java
+++ app/src/main/java/com/exerciseapplication/finalexerciseapplication/Exercise.java
@@ -6,7 +6,7 @@ package com.exerciseapplication.finalexerciseapplication;
 // An exercise Name       ** Required **
 // A Muscle Group that the exercise targets
 // A description of the exercise
-// A image / demonstration of the exercise
+// A image / demonstration of the exercise // Will need to be the name of image as objects cannot store images
 // A potential link to a video showing the exercise ** bonus not needed **
 // A rep count - Will be manipulated by user
 // A set count - Will be manipulated by user
