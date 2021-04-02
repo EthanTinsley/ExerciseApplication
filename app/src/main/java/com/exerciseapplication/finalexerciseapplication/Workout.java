1dd616185094d2e9a7ef45ee28b28a9e7ecd629c
Ethan <bigetinsley@yahoo.com>
2021-04-01 23:19:33 -0400

    Created a Class to store Workout object

---
+++ app/src/main/java/com/exerciseapplication/finalexerciseapplication/Workout.java
@@ -0,0 +1,25 @@
+package com.exerciseapplication.finalexerciseapplication;
+
+// This class will store the workout object that is referenced and used throughout the rest of the app.
+// Class should contain a way to store an ?? Unlimited amount of exercises ?? in a linked list.
+// Alternatively could use an Array for easier manipulation and referencing. Must declare a size however.
+// -- Reguardless of Data Structure --
+// Workout object should hold a collection of exercises that can use defined methods
+// Workout method should be able to go to the next exercise in the order
+// Workout method should be able to recall the previous exercise
+// Workout method should allow the repositioning of exercises ( reordering )
+// Workout method should allow the deletion and reordering ( fill the null slot ) of an exercise
+// Workout method should be able to insert a new exercise to the workout ( override to accept a specific position , default to end of workout )
+// Workout should have a previous, current, next exercise ( regardless of array or linked list )
+// Workout should have a title
+// Workout should have a boolean to track status ( Active , Exercise )
+// Workout should have a variable to track how long workout lasts
+// Workout should have a date field that is filled in when a workout starts
+
+public class Workout {
+
+
+
+
+
+}
