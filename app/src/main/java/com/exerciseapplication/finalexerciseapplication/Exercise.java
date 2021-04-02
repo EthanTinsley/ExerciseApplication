8c3da388aaae79f6ebbe41f0290dd6ae11327715
Ethan <bigetinsley@yahoo.com>
2021-04-01 23:28:54 -0400

    Created a Class to store Exercise Object

---
+++ app/src/main/java/com/exerciseapplication/finalexerciseapplication/Exercise.java
@@ -0,0 +1,18 @@
+package com.exerciseapplication.finalexerciseapplication;
+
+// Exercise's are the cornerstone of the application
+// Will be the Object that is stored inside the Workout Object
+// Needs to contain the following:
+// An exercise Name       ** Required **
+// A Muscle Group that the exercise targets
+// A description of the exercise
+// A image / demonstration of the exercise
+// A potential link to a video showing the exercise ** bonus not needed **
+// A rep count - Will be manipulated by user
+// A set count - Will be manipulated by user
+
+
+public class Exercise {
+
+
+}
