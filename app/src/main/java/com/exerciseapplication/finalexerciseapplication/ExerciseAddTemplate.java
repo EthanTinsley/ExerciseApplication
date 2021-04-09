e4ca8a7399d6fedbead761aef4180896f3be6a64
Ethan <bigetinsley@yahoo.com>
2021-04-09 11:21:49 -0400

    Added a layout to add exercises with, will dynamically change

--- app/src/main/java/com/exerciseapplication/finalexerciseapplication/ExerciseAddTemplate.java
+++ app/src/main/java/com/exerciseapplication/finalexerciseapplication/ExerciseAddTemplate.java
@@ -3,11 +3,11 @@ package com.exerciseapplication.finalexerciseapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Exercises extends AppCompatActivity {
+public class ExerciseAddTemplate extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exercises);
+        setContentView(R.layout.activity_exercise_add_template);
     }
 }
