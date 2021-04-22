1c2b7bae83a3f2534ebe1c003cf25b8d0e218f5f
Ethan <bigetinsley@yahoo.com>
2021-04-22 09:17:56 -0400

    Added some comments

--- app/src/main/java/com/exerciseapplication/finalexerciseapplication/MainActivity.java
+++ app/src/main/java/com/exerciseapplication/finalexerciseapplication/MainActivity.java
@@ -13,7 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Create buttons and find thier resource values
+        // user Intents to pass app flow to next layout
+        // user intents to pass any value/data neccesarry inside button listeners
 
+        // Button to view CompletedWorkouts
         Button CompletedWorkouts = (Button) findViewById(R.id.PreviousWorkoutsButton);
         CompletedWorkouts.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Button to view the exercises without adding them to a workout
         Button BrowseExercise = (Button) findViewById(R.id.BrowseExercisesButton);
         BrowseExercise.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Button to start building a new Workout
         Button BuildWorkoutButton = (Button) findViewById(R.id.BuildWorkoutButton);
         BuildWorkoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Button to View all exisitng workouts
         Button SavedWorkoutButton = (Button) findViewById(R.id.ViewWorkoutsButton);
         SavedWorkoutButton.setOnClickListener(new View.OnClickListener(){
 
@@ -52,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Button to start a workout by selecting from one of the existing templates
         Button StartWorkoutButton = (Button) findViewById(R.id.MainStartWorkout);
         StartWorkoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
