258085c692e97b202479677a0048d9c066b74175
Ethan <bigetinsley@yahoo.com>
2021-04-25 17:58:01 -0400

    Hamstring + Calves Action listeners

--- app/src/main/java/com/exerciseapplication/finalexerciseapplication/CalvesExercises.java
+++ app/src/main/java/com/exerciseapplication/finalexerciseapplication/CalvesExercises.java
@@ -1,14 +1,169 @@
 package com.exerciseapplication.finalexerciseapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class CalvesExercises extends AppCompatActivity {
+
+    boolean BuildingWorkout ;
+    String WorkoutName ;
+    String ExerciseName ;
+    String ExerciseDescription ;
+    int imageID ;
+
+    protected Button LegPressCalfRaises ;
+    protected Button DonkeyCalfRasies ;
+    protected Button SeatedCalfRaises ;
+    protected Button SmithMachineCalfRaises ;
+    protected Button StandingBarbellCalfRaise ;
+    protected Button StandingDumbellCalfRaises ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calves_exercises);
+
+        //determine if user is currently building a workout
+        BuildingWorkout = getIntent().getBooleanExtra("BuildingWorkout",false);
+
+        // if building a workout assign workout title
+        if (BuildingWorkout == true) WorkoutName = getIntent().getStringExtra("WorkoutTitle");
+
+        LegPressCalfRaises = (Button) findViewById(R.id.LegPressCalfRaiseButton);
+        LegPressCalfRaises.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.LegPressCalfRaise).toString();
+                ExerciseDescription = getString(R.string.LegPressCalfRaiseDescription).toString();
+                imageID = R.drawable.legpresscalfraise;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
+        DonkeyCalfRasies = (Button) findViewById(R.id.DonkeyCalfRaiseButton);
+        DonkeyCalfRasies.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.DonkeyCalfRaise).toString();
+                ExerciseDescription = getString(R.string.DonkeyCalfRaiseDescription).toString();
+                imageID = R.drawable.donkeycalfraise;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
+        SeatedCalfRaises = (Button) findViewById(R.id.SeatedCalfRaiseButton);
+        SeatedCalfRaises.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.SeatedCalfRaise).toString();
+                ExerciseDescription = getString(R.string.SeatedCalfRaiseDescription).toString();
+                imageID = R.drawable.seatedcalfraise;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
+        SmithMachineCalfRaises = (Button) findViewById(R.id.SmithMachineCalfRaiseButton);
+        SmithMachineCalfRaises.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.SmithMachineCalfRaise).toString();
+                ExerciseDescription = getString(R.string.SmithMachineCalfRaiseDescription).toString();
+                imageID = R.drawable.smithmachinecalfraise;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
+        StandingBarbellCalfRaise = (Button) findViewById(R.id.StandingBarbellCalfRaiseButton);
+        StandingBarbellCalfRaise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.StandingBarbbellCalfRaise).toString();
+                ExerciseDescription = getString(R.string.StandingBarbbellCalfRaiseDescription).toString();
+                imageID = R.drawable.standingbarbellcalfraise;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
+        StandingDumbellCalfRaises = (Button) findViewById(R.id.StandingDumbbellCalfRaiseButton);
+        StandingDumbellCalfRaises.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.StandingDumbbellCalfRaise).toString();
+                ExerciseDescription = getString(R.string.StandingDumbbellCalfRaiseDescription).toString();
+                imageID = R.drawable.standingdumbellcalfraise;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
 
     }
 
--- app/src/main/java/com/exerciseapplication/finalexerciseapplication/HamstringExercises.java
+++ app/src/main/java/com/exerciseapplication/finalexerciseapplication/HamstringExercises.java
@@ -1,14 +1,171 @@
 package com.exerciseapplication.finalexerciseapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class HamstringExercises extends AppCompatActivity {
+
+    boolean BuildingWorkout ;
+    String WorkoutName ;
+    String ExerciseName ;
+    String ExerciseDescription ;
+    int imageID ;
+
+    protected Button StandingLunge ;
+    protected Button LegCurl ;
+    protected Button RomanianDeadlift ;
+    protected Button SeatedLegCurl ;
+    protected Button ElevatedLegCurl ;
+    protected Button SingleLegHamstringCurl ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hamstring_exercises);
+
+
+        //determine if user is currently building a workout
+        BuildingWorkout = getIntent().getBooleanExtra("BuildingWorkout",false);
+
+        // if building a workout assign workout title
+        if (BuildingWorkout == true) WorkoutName = getIntent().getStringExtra("WorkoutTitle");
+
+        StandingLunge = (Button) findViewById(R.id.StandingLungeButton);
+        StandingLunge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.StandingLunge).toString();
+                ExerciseDescription = getString(R.string.StandingLungeDescription).toString();
+                imageID = R.drawable.standinglunge;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
+        LegCurl = (Button) findViewById(R.id.LegCurlButton);
+        LegCurl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.LegCurl).toString();
+                ExerciseDescription = getString(R.string.LegCurlDescription).toString();
+                imageID = R.drawable.legcurl;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
+        RomanianDeadlift = (Button) findViewById(R.id.RomanianDeadliftButton);
+        RomanianDeadlift.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.RomanianDeadlift).toString();
+                ExerciseDescription = getString(R.string.RomanianDeadliftDescription).toString();
+                imageID = R.drawable.romaniandeadlift;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
+        SeatedLegCurl = (Button) findViewById(R.id.SeatedLegCurlButton);
+        SeatedLegCurl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.SeatedLegCurl).toString();
+                ExerciseDescription = getString(R.string.SeatedLegCurlDescription).toString();
+                imageID = R.drawable.seatedlegcurl;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
+        ElevatedLegCurl = (Button) findViewById(R.id.ElevatedDeadliftButton);
+        ElevatedLegCurl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.ElevatedDeadlift).toString();
+                ExerciseDescription = getString(R.string.ElevatedDeadliftDescription).toString();
+                imageID = R.drawable.elevateddeadlift;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
+        SingleLegHamstringCurl = (Button) findViewById(R.id.SingleLegHamstringCurlButton);
+        SingleLegHamstringCurl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.SingleLegHamstringCurl).toString();
+                ExerciseDescription = getString(R.string.SingleLegHamstringCurlDescription).toString();
+                imageID = R.drawable.singleleghamstringcurl;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
 
     }
 
