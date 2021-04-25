a3856a7eb55eec5b4fa62bc2fbdbccf543653154
Ethan <bigetinsley@yahoo.com>
2021-04-25 17:43:42 -0400

    Glutes Action listeners

--- app/src/main/java/com/exerciseapplication/finalexerciseapplication/GluteExercises.java
+++ app/src/main/java/com/exerciseapplication/finalexerciseapplication/GluteExercises.java
@@ -1,15 +1,147 @@
 package com.exerciseapplication.finalexerciseapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class GluteExercises extends AppCompatActivity {
+
+    boolean BuildingWorkout ;
+    String WorkoutName ;
+    String ExerciseName ;
+    String ExerciseDescription ;
+    int imageID ;
+
+    protected Button BarbellHipThrusts;
+    protected Button CableKickBacks;
+    protected Button GluteBlaster ;
+    protected Button GluteBridge ;
+    protected Button MachineHipAbduction ;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glutes_exercises);
 
+        //determine if user is currently building a workout
+        BuildingWorkout = getIntent().getBooleanExtra("BuildingWorkout",false);
+
+        // if building a workout assign workout title
+        if (BuildingWorkout == true) WorkoutName = getIntent().getStringExtra("WorkoutTitle");
+
+        BarbellHipThrusts = (Button) findViewById(R.id.BarbellHipThrustButton);
+        BarbellHipThrusts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.BarbbellHipThrust).toString();
+                ExerciseDescription = getString(R.string.BarbellHipThrustDescription).toString();
+                imageID = R.drawable.barbellhipthrust;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
+        CableKickBacks = (Button) findViewById(R.id.CableKickbackButton);
+        CableKickBacks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.CableKickback).toString();
+                ExerciseDescription = getString(R.string.CableKickbackDescription).toString();
+                imageID = R.drawable.cablekickbacks;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
+        GluteBlaster = (Button) findViewById(R.id.GluteBlasterButton);
+        GluteBlaster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.GluteBlaster).toString();
+                ExerciseDescription = getString(R.string.GluteBlasterDescription).toString();
+                imageID = R.drawable.gluteblaster;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
+        GluteBridge = (Button) findViewById(R.id.GluteBridgeButton);
+        GluteBridge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.GluteBridge).toString();
+                ExerciseDescription = getString(R.string.GluteBridgeDescription).toString();
+                imageID = R.drawable.glutebridge;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
+        MachineHipAbduction = (Button) findViewById(R.id.MachineHipAbductionButton);
+        MachineHipAbduction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.MachineHipAbduction).toString();
+                ExerciseDescription = getString(R.string.MachineHipAbductionDescription).toString();
+                imageID = R.drawable.machinehipabduction;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
     }
 
 
