42e2a947b0671e8a67cbae0534af6f5cb83135de
Ethan <bigetinsley@yahoo.com>
2021-04-25 16:59:43 -0400

    Forearms Action listeners

--- app/src/main/java/com/exerciseapplication/finalexerciseapplication/ForearmExercises.java
+++ app/src/main/java/com/exerciseapplication/finalexerciseapplication/ForearmExercises.java
@@ -1,9 +1,23 @@
 package com.exerciseapplication.finalexerciseapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ForearmExercises extends AppCompatActivity {
+
+    boolean BuildingWorkout ;
+    String WorkoutName ;
+    String ExerciseName ;
+    String ExerciseDescription ;
+    int imageID ;
+
+    protected Button BarbellHolds ;
+    protected Button Gripper ;
+    protected Button PlatePinch ;
+    protected Button FarmerCarries ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +25,99 @@ public class ForearmExercises extends AppCompatActivity {
         setContentView(R.layout.activity_forearm_exercises);
 
 
+        //determine if user is currently building a workout
+        BuildingWorkout = getIntent().getBooleanExtra("BuildingWorkout",false);
+
+        // if building a workout assign workout title
+        if (BuildingWorkout == true) WorkoutName = getIntent().getStringExtra("WorkoutTitle");
+
+        BarbellHolds = (Button) findViewById(R.id.BarbellHoldsButton);
+        BarbellHolds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.BarbellHolds).toString();
+                ExerciseDescription = getString(R.string.BarbellHoldsDescription).toString();
+                imageID = R.drawable.barbellholds;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
+        Gripper = (Button) findViewById(R.id.HandGripButton);
+        Gripper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.HandGripper).toString();
+                ExerciseDescription = getString(R.string.HandGripperDescription).toString();
+                imageID = R.drawable.handgripper;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
+        PlatePinch = (Button) findViewById(R.id.PlatePinchButton);
+        PlatePinch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.PlatePinch).toString();
+                ExerciseDescription = getString(R.string.PlatePinchDescription).toString();
+                imageID = R.drawable.platepinch;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
+
+        FarmerCarries = (Button) findViewById(R.id.FarmerscarryTitle);
+        FarmerCarries.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Exercise = new Intent(view.getContext() , ExerciseAddTemplate.class) ;
+
+                if (BuildingWorkout == true) {
+                    Exercise.putExtra("WorkoutName", WorkoutName);
+                    Exercise.putExtra("BuildingWorkout", BuildingWorkout);
+                }
+
+                ExerciseName = getString(R.string.FarmersCarries).toString();
+                ExerciseDescription = getString(R.string.FarmersCarriesDescription).toString();
+                imageID = R.drawable.farmercarries;
+
+                Exercise.putExtra("ExerciseName", ExerciseName);
+                Exercise.putExtra("ExerciseDescription", ExerciseDescription);
+                Exercise.putExtra("ExerciseImage", imageID);
+                startActivityForResult(Exercise,0);
+            }
+        });
 
     }
 
